307797711b2a09020cedc5265cbe25e648a8769c
techkee <nathaliadrealtor@gmail.com>
2022-11-07 04:00:37 -0600

    P done

--- app.js
+++ app.js
@@ -12,17 +12,3 @@ function() {
     document.querySelector('.bg-modal').style.display = 'none';
 
 });
-
-
-document.getElementById('button').addEventListener('click',
-function() {
-
-  document.querySelector('.bg-modal').style.display = 'flex';
-});
-
-document.querySelector('.close').addEventListener('click',
-function() {
-
-    document.querySelector('.bg-modal').style.display = 'none';
-
-});
